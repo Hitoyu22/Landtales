34f3ccc1fc731123583dacfdb229fe453355fec7
Rémy THIBAUT <131862821+Hitoyu22@users.noreply.github.com>
2024-06-15 20:20:38 +0200

    Update config.php

--- Structure/Bdd/config.php
+++ Structure/Bdd/config.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $bdd = new PDO('mysql:host=landtales.freeddns.org;dbname=landtales', 'landtales', 'Secret');
+    $bdd = new PDO('mysql:host=localhost;dbname=landtales', 'landtales', 'Secret');
 }
 catch(Exception $e)
 {
